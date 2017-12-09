c978438ca48dd58c4b3b9e8342593345da414e04
takahirochiku <tik.chiku@gmail.com>
2017-12-09 19:16:04 +0900

    ver1.0.0
    //when sensor sensed the electorocity and send HIGH signal, Nefry triggered IFTTT to send message through LINE.

---
+++ nefry_touch_main/nefry_touch_main.ino
@@ -0,0 +1,27 @@
+#include <Nefry.h>
+#include <NefryIFTTT.h>
+String Event, SecretKey;
+int counter =0;                       //送信データのカウンタ
+
+void setup() {
+  Nefry.setStoreTitle("SecretKey",0); //Nefry DataStoreのタイトルを指定
+  Nefry.setStoreTitle("Event",1);     //Nefry DataStoreのタイトルを指定
+  SecretKey = Nefry.getStoreStr(0);   //Nefry DataStoreからデータを取得
+  Event = Nefry.getStoreStr(1);       //Nefry DataStoreからデータを取得
+// Nefry.enableSW();                   //SW有効化
+  Nefry.println("Nefry Setup!!");
+
+  pinMode(A2,INPUT_PULLUP);//入力モード切り替え
+}
+
+void loop() {
+  if(digitalRead(A2)==HIGH) {               //センサーが静電気を検知した時
+    bool sendData = IFTTT.send(Event, SecretKey,"Nefry",(String)(micros()/1000000)+"秒");//IFTTTにデータを送信
+                                      //Value1:Nefry,Value2:Nefryが起動してからの秒数,Value3:送信カウンタ
+     Nefry.println("A2 HIGH");
+    if (!sendData) {//IFTTTにデータを送信が成功したか失敗したかの判定
+      Nefry.setLed(255, 0, 0);        //Errの時、赤色点灯
+    }
+    Nefry.ndelay(1000);               //送信後1秒間待つ
+  }
+}
